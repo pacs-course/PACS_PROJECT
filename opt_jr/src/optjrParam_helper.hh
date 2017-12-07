157524f2a8d0ae47ca76689dfb8bf7658349c693
Davide <davide.burba@mail.polimi.it>
2017-12-07 19:18:40 +0100

    Improved makefile and files organization

--- opt_jr/src/optjrParam_helper.hh
+++ opt_jr/src/optjrParam_helper.hh
@@ -2,6 +2,31 @@
 #define HELPER_HH
 
 
+/************************************************************
+VARIABILI GLOBALI UTILI PER CREARE OGGETTO DI CLASSE
+OPTJRPARAMETER, CHE MEMORIZZA PARAMETRI PASSATI DA COMMAND_LINE
+************************************************************/
+
+
+
+#define ARGS 8 //number of arguments from command line
+
+#define FILENAME "-f="
+#define NUM_N "-n="
+#define LIST_LIMIT "-k="
+#define DEBUG "-d="
+#define MAX_ITERATIONS "-i="
+#define SIMULATOR "-s="
+#define GLOBAL_FO_CALCULATION "-g"
+
+#define NUMBER 0
+#define STRING 1
+#define YES_NO 2
+
+#define NO 0
+#define YES 1
+
+
 
 /*************************************************************
     A FEW OPTJRPARAMETERS HELPER FUNCTION
